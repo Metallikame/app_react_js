32cab1e5a720ba1401c814f5b947b8651ecb0fb6
Metallikame <rex2644@outlook.com>
2022-12-16 15:39:48 +0100

    app fonctionnelle

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import './index.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+
+    // Constructor
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            items: [],
+            DataisLoaded: false
+        };
+    }
+
+    // ComponentDidMount is used to
+    // execute the code
+    componentDidMount() {
+        fetch("https://jsonplaceholder.typicode.com/users")
+            .then((res) => res.json())
+            .then((json) => {
+                this.setState({
+                    items: json,
+                    DataisLoaded: true
+                });
+            })
+    }
+
+    render() {
+        const {DataisLoaded, items} = this.state;
+        if (!DataisLoaded) return <div>
+            <h1> Un peu de patience les données son en train de charger... </h1></div>;
+
+        return (
+            <div className="App">
+                <h1> Récupération des données </h1>
+                {
+                    items.map((item) => (
+                        <ol key={item.id}>
+                            Pseudo : {item.username} // Nom : {item.name} // Adresse mail : {item.email}
+                        </ol>
+                    ))
+                }
+            </div>
+        );
+    }
 }
 
 export default App;
